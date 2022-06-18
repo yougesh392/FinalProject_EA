a57ef3641b3e3f9f16b7fcb0dc076ee4ec4c92da
Yougesh Kumar <ypinjani@gmail.com>
2022-06-18 01:23:45 -0500

    Last commit for the project

--- authentication-service/src/main/java/edu/miu/cs544/authenticationservice/service/impl/UaaService.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.cs544.authenticationservice.service.impl;
-
-import edu.miu.cs544.authenticationservice.model.LoginRequest;
-import edu.miu.cs544.authenticationservice.model.LoginResponse;
-
-public interface UaaService {
-    LoginResponse login(LoginRequest loginRequest) throws Exception;
-}
