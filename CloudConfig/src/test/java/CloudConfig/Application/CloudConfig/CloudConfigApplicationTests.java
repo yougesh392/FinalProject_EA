8a8f9903a32e76b9aa2144b592665cf1a12a6195
Yougesh Kumar <ypinjani@gmail.com>
2022-08-16 21:05:21 -0500

    Added the Eureka and the clod config

---
+++ CloudConfig/src/test/java/CloudConfig/Application/CloudConfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package CloudConfig.Application.CloudConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
